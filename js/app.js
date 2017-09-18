519e6462b48661b21fba196da77cb385312e7694
Forrest Wilson <forrest.wilson@me.com>
2017-09-18 20:56:36 +1200

    working mobile popout nav

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
+    
     /////////////////////////
     //// Click Functions ////
     /////////////////////////
 
+
+    // Sign In Click Functions
     $("#signinButton").click(function() {
         $("#overlay").fadeIn(300);
     });
 
     $("#closeButton").click(function() {
         $("#overlay").fadeOut(300);
+    });
+
+    // Scene Switching
+    $(".scene").click(function() {
+        $(".scene").removeClass("isActiveScene");
+        $(this).addClass("isActiveScene");
+    });
+
+    // Navigation popout
+    $("#hamburgerButton").click(function() {
+        $("#mobileNav").toggleClass("isActiveMobileNav");
     });
 });
