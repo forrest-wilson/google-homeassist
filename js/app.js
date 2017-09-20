7ed390f15c8ada4ec2ce636a1e9af5e08f9bbad3
Forrest Wilson <forrest.wilson@me.com>
2017-09-20 13:56:03 +1200

    added #heaterOverlay fade out and slider function

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
 
     $("#closeButton").click(function() {
         $("#signinOverlay").fadeOut(300);
+        $("#heaterOverlay").fadeOut(300);
     });
 
     // Scene Switching
@@ -32,4 +33,13 @@ $(document).ready(function() {
         $(this).parent().toggleClass("isOnDevice");
         $(this).toggleClass("isOnSwitch");
     });
+
+    // Print slider value to the innerHTML of the slider label
+    var slider = document.getElementById("tempSlider");
+    var output = document.getElementById("tempOutput");
+    output.innerHTML = slider.value;
+    
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+    }
 });
